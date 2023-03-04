1bb285317d3ec87f3d1d870294496528c9e15eb1
FelipeFCamboim <125658044+FelipeFCamboim@users.noreply.github.com>
2023-03-04 20:01:13 -0300

    Update and rename main.py to smartfolio.py

---
+++ smartfolio.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import fill
+import get_prices
+import get_risk
+import log_return
+import markowitz
+import graphics
+import view
+
+def main():
+    data = get_prices.get_prices(tickers, start, end, interval)
+
+
+if __name__ == "__main__":
+    main()
