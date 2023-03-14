c93df0e2737ed045afd83ee1341f865201415921
FelipeFCamboim <125658044+FelipeFCamboim@users.noreply.github.com>
2023-03-14 20:56:53 -0300

    Update view.py

--- view.py
+++ view.py
@@ -6,12 +6,15 @@ import get_risk
 import yfinance as yf
 import get_data
 from datetime import datetime, date
-#import get_returns
+import get_returns
+import plotly.express as px
+
 
 app = Dash(__name__)
 
 tickers = get_data.get_tickers()
 intervals = ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo']
+
 
 ###### LAYOUT ############
 
@@ -24,7 +27,7 @@ app.layout = html.Div([
 
     html.Div(children=[
         html.Br(),
-        html.Label('Select your ticker'),
+        html.Label('Select multiple tickers'),
         dcc.Dropdown(tickers,
                      multi=True, 
                      id='tickers_dropdown'
@@ -46,23 +49,19 @@ app.layout = html.Div([
         html.Label('Select interval'),
         dcc.Dropdown(intervals, id='intervals_dropdown'),
 
-        html.Button('Submit', id='submit_button')
+        html.Button('Submit', id='submit_button'),
     
     ], style={'padding': 10, 'flex': 1, 'textAlign':'center'}),
 
     html.Div(id='selected_tickers', className='table'),    
+    dcc.Graph(id='grafico')
     ],
-
-    #html.Div(id = "teste",
-    #children=[
-    #    html.Br(),
-    #    html.Label('Returns table'),
-    #    table(returns)])],
 
     style={'display': 'flex', 'flex-direction': 'column'})
 
 @app.callback(
-    Output('selected_tickers', 'children'),
+    [Output('selected_tickers', 'children'),
+    Output('grafico', 'figure')],
     [Input('submit_button','n_clicks')],
     [State('tickers_dropdown', 'value'),
     State('start_date', 'value'),
@@ -71,29 +70,25 @@ app.layout = html.Div([
     prevent_initial_call=True
 )
 def get_some_prices(n, selected_tickers, start, end, interval):
-    prices = yf.download(selected_tickers, start=start, end=end, interval=interval)['Adj Close'].fillna(0).reset_index()
-    col = [{'name': i, 'id': i} for i in prices.columns]
+    prices = yf.download(selected_tickers, start=start, end=end, interval=interval)['Adj Close'].fillna(0)
+    prices2 = prices.reset_index()
+    col = [{'name': i, 'id': i} for i in prices2.columns]
+        
     table = dash_table.DataTable(
             id='table',
             columns = col,
             page_size= 6,
             style_cell={'width': '45'},
-            data = prices.to_dict('records')
-    )
-    return table
+            data = prices2.to_dict('records')
+            )
+    
+    log_returns = np.log(prices).diff().fillna(0).reset_index()
+    return_cumreturn = log_returns.cumsum()
 
-'''
-    df_chart = df.groupby('Name').sum()
-    return [
-        dt.DataTable(
-            rows=df_chart.to_dict('rows'),
-            columns=df_chart.columns,
-            row_selectable=True,
-            filterable=True,
-            sortable=True,
-            selected_row_indices=list(df_chart.index),  # all rows selected by default
-            id='3'
-        )
-'''
+    fig = px.line(log_returns, x='Date', y=selected_tickers)
+
+    return table, fig
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
