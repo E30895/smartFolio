020021f068e8a658e025dd301e95fbae2b834b32
FelipeFCamboim <125658044+FelipeFCamboim@users.noreply.github.com>
2023-03-23 00:56:49 -0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
+import datetime
+import re
 import yfinance as yf
 import pandas as pd
-import tabula
 
 
 def get_tickers() -> list:
@@ -15,7 +16,7 @@ def get_tickers() -> list:
     return tickers_list
 
 
-def get_prices(tickers: str | list, start: str, end: str, interval: str) -> pd.Series | pd.DataFrame:
+def get_prices(selected_tickers: list, start: str, end: str, interval: str) -> pd.DataFrame:
     '''
     This function download the prices of a list of tickers and return 
     a pandas.Series (if the "tickers" parameter is a string) 
@@ -26,16 +27,35 @@ def get_prices(tickers: str | list, start: str, end: str, interval: str) -> pd.S
         tickers : list
             List of tickers to download
         start: str
-            Download start date string (YYYY-MM-DD) or _datetime.
+            Download start date string (YYYY-MM-DD).
             Default is 1900-01-01
         end: str
-            Download end date string (YYYY-MM-DD) or _datetime.
+            Download end date string (YYYY-MM-DD).
             Default is now
         interval : str
-            Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+            Valid intervals: 1d,5d,1wk,1mo,3mo
             Intraday data cannot extend last 60 days
     '''
     
-    prices = yf.download(tickers, start, end, interval)['Adj Close'].fillna(0)
+    start_splitted = list(map(int, re.split('-', start, 2)))
+    end_splitted = list(map(int, re.split('-', end, 2)))
+    
+    start_date = str(int(datetime.datetime(start_splitted[0], start_splitted[1], start_splitted[2], 21).timestamp()))
+    end_date = str(int(datetime.datetime(end_splitted[0], end_splitted[1], end_splitted[2], 21).timestamp()))
+
+    if len(selected_tickers) == 1:
+        url = f'https://query1.finance.yahoo.com/v7/finance/download/{selected_tickers}?period1={start_date}&period2={end_date}&interval={interval}&events=history&includeAdjustedClose=true'
+        prices = pd.read_csv(url)[['Date', 'Adj Close']].astype({'Date': 'string'})
+    else:
+        url = f'https://query1.finance.yahoo.com/v7/finance/download/{selected_tickers[0]}?period1={start_date}&period2={end_date}&interval={interval}&events=history&includeAdjustedClose=true'
+        prices = pd.read_csv(url)['Date'].astype('string')
+        
+        for ticker in selected_tickers:
+            url = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={start_date}&period2={end_date}&interval={interval}&events=history&includeAdjustedClose=true'
+            ticker_prices = pd.read_csv(url)[['Date', 'Adj Close']].astype({'Date': 'string'})
+            prices = pd.merge(prices, ticker_prices, how='outer', on='Date')
+    
+    
+    prices.columns = ['Date'] + selected_tickers
 
     return prices
