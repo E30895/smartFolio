eb7a901c66b38827e5ea9f6c5c4e1972b3eac357
FelipeFCamboim <125658044+FelipeFCamboim@users.noreply.github.com>
2023-03-28 19:51:59 -0300

    Update model_tickers.py

--- model_tickers.py
+++ model_tickers.py
@@ -1,1 +1,12 @@
+import requests
 
+def get_tickers() -> list:
+    ''' Get many brazilians stock tickers from a text file and
+        return a list.
+    '''
+    url = 'https://github.com/E30895/smartFolio/raw/main/tickers_list.txt'
+    request = requests.get(url)
+    tickers_list = request.text.split()
+    request.close()
+
+    return tickers_list
