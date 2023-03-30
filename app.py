71a254fd582b7a652a332325beeb8209a033a8d9
FelipeFCamboim <125658044+FelipeFCamboim@users.noreply.github.com>
2023-03-30 17:43:11 -0300

    Update and rename controller.py to app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 from dash import Dash, html, dcc, dash_table
 from dash.dependencies import Output, Input, State
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import model_financial
-import model_econometrics
+import model_tickers
 import view
 
 """
@@ -24,7 +27,9 @@ app.layout = layout
 
 @app.callback(
     [Output('selected_tickers', 'children'),
-    Output('grafico', 'figure')],
+    Output('returns_fig', 'figure'),
+    Output('weights_table', 'children'),
+    Output('efficient_frontier', 'figure')],
     [Input('submit_button','n_clicks')],
     [State('tickers_dropdown', 'value'),
     State('start_date', 'value'),
@@ -34,10 +39,22 @@ app.layout = layout
 )
 def main(n_clicks, selected_tickers, start, end, interval):
     prices = model_tickers.get_prices(selected_tickers, start=start, end=end, interval=interval)
-    returns = model_financial.log_return(prices)
-    table = view.table(returns)
-    fig = view.fig_line(returns, selected_tickers)
-    return table, fig
+    returns = model_financial.log_return(prices)    
+    
+    table_returns = view.table(returns)
+    fig_returns = view.fig_line(returns, selected_tickers)
+
+    wallets = model_financial.generate_wallets(returns.iloc[:, 1:])
+    best_porfolio, max_sharpe = model_financial.best_porfolio(wallets)
+
+    # Transform best_porfolio np.array into a pd.DataFrame
+    best_porfolio = pd.DataFrame(data=best_porfolio, columns=['Pesos'], index=selected_tickers).reset_index()
+    best_porfolio.columns = ['Ativos', 'Pesos']
+
+    table_weights = view.table(best_porfolio)
+    fig_wallets = view.plot_efficient_frontier(wallets, max_sharpe)
+
+    return table_returns, fig_returns, table_weights, fig_wallets
 
 
 if __name__ == '__main__':
